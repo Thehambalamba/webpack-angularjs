1cf291faf4ceb31ab7b836714ca2747477ccdd39
Nikola Pervic <nikolapervic@me.com>
2017-10-05 02:30:27 +0200

    Configured babel, file-loader(font-img), style loaders for scss, html-loader. The /dist folder now contains our html,css (scss-styling) and js boundle.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,60 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+const config = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /nodemodules/
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+            use: [{
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader"
+            }],
+            // use style-loader in development
+            fallback: "style-loader"
+        })
+      },
+      {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+       },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader'
+         ]
+       }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+       title: 'Output Management'
+    })
+  ]
 };
+
+module.exports = config;
